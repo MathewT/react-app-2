5075a3adb1f7e4c5d7243434857a473027988a2e
Mathew Thomas <mathewt@bitcloset.com>
2020-11-24 15:18:11 -0500

    made App a Component; introduced state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+import React, {Component} from 'react';
+
 import './App.css';
 import Dog from './Dog/Dog';
 
-function App() {
-  return (
-    <div className="App">
-      <h3>hello, React world of dogs.</h3>
-      <Dog name="Max" age="6" />
-      <Dog name="Kirby" age="8">I am a sweet dog.</Dog>
-      <Dog name="Lola" age="6" />
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    actionClickedCounter: 0,
+    dogs: [
+      {
+        name: "Bowser",
+        age: 7
+      },
+      {
+        name: "Jessie",
+        age: 2
+      },
+      {
+        name: "Felix",
+        age: 3
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+       <h3>hello, React Component world of dogs.</h3>
+       <button>Action Clicked: {this.state.actionClickedCounter}</button>
+       <Dog name="Max" age="6" />
+       <Dog name="Kirby" age="8">I am a sweet dog.</Dog>
+       <Dog name="Lola" age="6" />
+      </div>
+    );
+  };
+};
 
 export default App;
