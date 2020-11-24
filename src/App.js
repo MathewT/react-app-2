37a031fb6cdaccc2d6f3e8afafac514c94b4ea15
Mathew Thomas <mathewt@bitcloset.com>
2020-11-24 14:21:35 -0500

    added the Dog component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-// import logo from './logo.svg';
+import { render } from 'react-dom';
 import './App.css';
+import Dog from './Dog/Dog';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello, world</h1>
+      <h3>hello, React world</h3>
+      <Dog />
     </div>
   );
 }
---
+++ src/Dog/Dog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const dog = () => {
+  return (
+    <p>first dog</p>
+  )
+}
+
+export default dog;
